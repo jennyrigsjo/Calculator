d23eb469ed3c48eb86291d311c5fd0c2f7168ace
jenny <jenny.rigsjo@live.com>
2022-08-04 14:19:46 +0200

    Move Calculator and CalculatorController classes to separate files in Calculator project.

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public Dictionary<string, string> Operators { get; private set; } = new Dictionary<string, string>()
+        {
+            {"add", "+"},
+            {"subtract", "-"},
+            {"multiply", "*"},
+            {"divide", "/"},
+            {"remainder", "%"},
+        };
+
+        public string Op { get; set; } = string.Empty;
+
+        public double Num1 { get; set; } = double.NaN;
+
+        public double Num2 { get; set; } = double.NaN;
+
+        public string Calculate()
+        {
+            string result;
+
+            switch (Op)
+            {
+                case "+":
+                    result = Add();
+                    break;
+                case "-":
+                    result = Subtract();
+                    break;
+                case "*":
+                    result = Multiply();
+                    break;
+                case "/":
+                    result = Divide();
+                    break;
+                case "%":
+                    result = Remainder();
+                    break;
+                default:
+                    result = $"Invalid operation: {Op}";
+                    break;
+            }
+
+            return result;
+        }
+
+        public void Reset()
+        {
+            Op = string.Empty;
+            Num1 = double.NaN;
+            Num2 = double.NaN;
+        }
+
+        public bool Ready()
+        {
+            return (Op != string.Empty && !double.IsNaN(Num1) && !double.IsNaN(Num2));
+        }
+
+        private string Add()
+        {
+            double result = Math.Round(Num1 + Num2, 2);
+            return $"{Num1} + {Num2} = {result}";
+        }
+
+        private string Subtract()
+        {
+            double result = Math.Round(Num1 - Num2, 2);
+            return $"{Num1} - {Num2} = {result}";
+        }
+
+        private string Multiply()
+        {
+            double result = Math.Round(Num1 * Num2, 2);
+            return $"{Num1} x {Num2} = {result}";
+        }
+
+        private string Divide()
+        {
+            try
+            {
+                if (Num2 == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+
+                double result = Math.Round(Num1 / Num2, 2);
+                return $"{Num1} / {Num2} = {result}";
+            }
+            catch (DivideByZeroException)
+            {
+                return $"Division by {Num2}!";
+            }
+        }
+
+        private string Remainder()
+        {
+            try
+            {
+                if (Num2 == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+
+                double result = Math.Round(Num1 % Num2, 2);
+                return $"{Num1} % {Num2} = {result}";
+            }
+            catch (DivideByZeroException)
+            {
+                return $"Division by {Num2}!";
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator/Controller.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorController
+    {
+        public CalculatorController()
+        {
+            Menu();
+
+            while (Calculate)
+            {
+                string[] prompts = { "First number", "Operator", "Second number" };
+
+                foreach (string prompt in prompts)
+                {
+                    var input = "";
+
+                    while (string.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine();
+                        Console.Write($"{prompt}: ");
+                        input = Console.ReadLine();
+                    }
+
+                    if (prompt == "First number" && IsNumber(input))
+                    {
+                        Calc.Num1 = double.Parse(input);
+                    }
+
+                    else if (prompt == "Second number" && IsNumber(input))
+                    {
+                        Calc.Num2 = double.Parse(input);
+                    }
+
+                    else if (prompt == "Operator" && IsOperator(input))
+                    {
+                        Calc.Op = input;
+                    }
+
+                    else
+                    {
+                        ChooseOption(input);
+                        Calc.Reset();
+                        break;
+                    }
+
+                }
+
+                if (Calc.Ready())
+                {
+                    string result = Calc.Calculate();
+
+                    Console.WriteLine();
+                    Console.WriteLine(result);
+
+                    Calc.Reset();
+                }
+            }
+        }
+
+        private Calculator _calc = new();
+        private Calculator Calc
+        {
+            get => _calc;
+        }
+
+        private bool _calculate = true;
+        private bool Calculate
+        {
+            get => _calculate;
+            set => _calculate = value;
+        }
+
+        private void Menu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*-*-*-*-*-*- Welcome to Calculator -*-*-*-*-*-*");
+            Console.WriteLine("When prompted, enter two numbers and an operator to calculate the result.");
+            Options();
+        }
+
+        private void Options()
+        {
+            Console.WriteLine();
+            Console.WriteLine("- - - Options- - -");
+            Console.WriteLine("'h' = help");
+            Console.WriteLine("'o' = options");
+            Console.WriteLine("'op' = available operators");
+            Console.WriteLine("'q' = quit");
+        }
+
+        private void Operators()
+        {
+            Console.WriteLine();
+            Console.WriteLine("- - - Possible operations - - -");
+
+            foreach (var op in Calc.Operators)
+            {
+                Console.WriteLine($"'{op.Value}' = {op.Key}");
+            }
+        }
+
+        private void ChooseOption(string choice)
+        {
+            switch (choice.ToLower())
+            {
+                case "h":
+                    Menu();
+                    break;
+                case "o":
+                    Options();
+                    break;
+                case "op":
+                    Operators();
+                    break;
+                case "q":
+                    Calculate = false;
+                    Console.WriteLine();
+                    Console.WriteLine("Program ended. Bye!");
+                    break;
+                default:
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid input!");
+                    break;
+            }
+        }
+
+        private bool IsNumber(string input)
+        {
+            return double.TryParse(input, out double _);
+        }
+
+        private bool IsOperator(string input)
+        {
+            return Calc.Operators.ContainsValue(input);
+        }
+    }
+}
